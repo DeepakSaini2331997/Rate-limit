1c5e505d62369ee71660b922d1607e9f119cd9ca
deepak <deepaksaini2331997@gmail.com>
2024-12-26 17:51:16 +0530

    handle user request from using redis cache

--- redis-rate-limit/config/env.js
+++ redis-rate-limit/config/env.js
@@ -1,6 +1,18 @@
+
 const dotenv = require("dotenv")
 
 dotenv.config()
 
 const RATELIMIT_DURATION_IN_SECOND = process.env.RATELIMIT_DURATION_IN_SECOND ||60
 const NUMBER_OF_REQUEST_ALLOWED = process.env.NUMBER_OF_REQUEST_ALLOWED || 5
+const REDIS_HOST = process.env.REDIS_HOST
+const REDIS_PORT = process.env.REDIS_PORT
+const REDIS_PASSWORD = process.env.REDIS_PASSWORD
+
+module.exports = {
+    RATELIMIT_DURATION_IN_SECOND,
+    NUMBER_OF_REQUEST_ALLOWED,
+    REDIS_HOST,
+    REDIS_PORT,
+    REDIS_PASSWORD
+}
--- redis-rate-limit/middleware/rate-limit.js
+++ redis-rate-limit/middleware/rate-limit.js
@@ -1,26 +1,54 @@
 const Redis = require("ioredis")
 const moment = require("moment")
-require('../config/env')
-const redisClient = new Redis({url:"redis://localhost:6379"})
+const { REDIS_HOST,REDIS_PASSWORD,REDIS_PORT,NUMBER_OF_REQUEST_ALLOWED,RATELIMIT_DURATION_IN_SECOND} = require('../config/env.js')
+//const redisClient = new Redis({url:"redis://localhost:6379"})
+//console.log(REDIS_HOST+'----'+REDIS_PORT+'-----'+REDIS_PASSWORD+'---'+NUMBER_OF_REQUEST_ALLOWED)
+
+const redisClient = new Redis({
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+    password: REDIS_PASSWORD
+});
+
+redisClient.on("connect",(err)=>{
+    if(err){
+        console.log('Redis Connect Error '+err)
+
+    }
+    console.log('Redis Connect Successfully')
+})
 
 const rateLimiter = async (req,res,next) =>{
     try{
-        const userId = req.headers["userId"] // insted for this we use ip address
+        const userId = req.headers["userid"] // insted for this we use ip address
+        //console.log(req.headers," my userid"+userId)
+
+        if(!userId){
+            return res.status(500).json({
+                success:false,
+                msg:'Token required!'
+            })
+        }
         const currentTime = moment().unix()
         const checkUserIdExit = await redisClient.hgetall(userId)
-        if(!checkUserIdExit){
-            await redisClient.hset(userId,{
+        //console.log(checkUserIdExit," check user exist")
+
+        if(Object.keys(checkUserIdExit).length === 0 ){
+            const result = await redisClient.hset(userId,{
                 "createdAt":currentTime,
                 "count":1
             })
+            //console.log(result," save result2")
             return next()
         }else{
             const diff = currentTime - checkUserIdExit["createdAt"] 
+            console.log(diff, " its difference")
             if(diff>RATELIMIT_DURATION_IN_SECOND){
-                await redisClient.hset(userId,{
+                const result = await redisClient.hset(userId,{
                     "createdAt":currentTime,
                     "count":1
                 })
+                //console.log(result," save result3")
                 return next()
             }
 
@@ -30,9 +58,10 @@ const rateLimiter = async (req,res,next) =>{
                     msg:"user-ratelimit exit"+NUMBER_OF_REQUEST_ALLOWED
                 })
             }else{
-                await redisClient.hset(userId,{
-                    "count":parseInt(checkUserIdExit["count"]+1)
+                const result = await redisClient.hset(userId,{
+                    "count":parseInt(checkUserIdExit["count"])+parseInt(1)
                 })
+                //console.log(result," save result4 ",checkUserIdExit["count"])
                 return next()
             }
         }
